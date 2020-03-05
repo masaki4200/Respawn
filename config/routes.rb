d3fca615ea5053ccada7be961548da6eb4ed8b65
masaki4200 <masa326420.4200@gmail.com>
2020-03-05 07:01:41 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions:      'devise/users/sessions',
+    passwords:     'devise/users/passwords',
+    registrations: 'devise/users/registrations'
+  }
+
+  
   namespace :users do
     get 'home/top'
   end
@@ -13,6 +20,5 @@ Rails.application.routes.draw do
   get 'users/edit'
   get 'users/withdraw'
   devise_for :admins
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

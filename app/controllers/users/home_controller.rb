add4d13b48404febe103364f665ba13a17ff4b0f
masaki4200 <masa326420.4200@gmail.com>
2020-03-30 11:42:46 +0000

    ランダムをmysqlの記述に変更

--- app/controllers/users/home_controller.rb
+++ app/controllers/users/home_controller.rb
@@ -1,8 +1,7 @@
 class Users::HomeController < ApplicationController
   def top
   	# @items = Item.order("RANDOM()").all
-
-  	#  ※ mysql時はこちらを使用する
+  	# ※ mysql時はこちらを使用する
   	@items = Item.order("RAND()").all
   end
 end
